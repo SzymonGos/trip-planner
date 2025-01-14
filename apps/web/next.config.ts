ef704eeb472dd18d9418752a6d93f37532fd3c99
Szymon <szymonngos@gmail.com>
2025-01-14 15:13:04 +0000

    create nx monorepo, cleanup

--- apps/web/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  env: {
-    GRAPHQL_API_URL: process.env.GRAPHQL_API_URL || "",
-  },
-};
-
-export default nextConfig;
