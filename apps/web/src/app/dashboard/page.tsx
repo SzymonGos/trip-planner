55ae6d4ab0e0bbbcfc7d64b1656c65778da87461
Szymon <szymonngos@gmail.com>
2025-01-25 15:23:38 +0000

    init

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const DashboardPage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
       <h2>Dashboard</h2>
     </div>
   );
