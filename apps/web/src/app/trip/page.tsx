4013cd197a1395ddefaa6e7a5e7f16ae9f250f25
Szymon <szymonngos@gmail.com>
2025-02-12 13:09:57 +0000

    minor

--- apps/web/src/app/trip/page.tsx
+++ apps/web/src/app/trip/page.tsx
@@ -1,5 +1,5 @@
 const Trip = () => (
-  <div className="mt-[73px] h-[calc(100vh-73px)]">
+  <div className="h-[calc(100vh-73px)]">
     <div className="flex h-full">
       <div className="w-full lg:w-[500px]">Left Column</div>
       <div className="hidden lg:block flex-1 border-l">Right Column</div>
