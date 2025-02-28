5b5d82d60297f399700a3c66d7fb8b7c60b75d4e
Szymon <szymonngos@gmail.com>
2025-02-28 11:10:23 +0000

    add fields to Trip, mutation

--- apps/keystone/src/schemas/Trip/Trip.ts
+++ apps/keystone/src/schemas/Trip/Trip.ts
@@ -5,6 +5,8 @@ import { allowAll } from '@keystone-6/core/access';
 export const Trip = list({
   fields: {
     title: text({ validation: { isRequired: true } }),
+    origin: text({ validation: { isRequired: true } }),
+    destination: text({ validation: { isRequired: true } }),
   },
   access: allowAll,
 });
