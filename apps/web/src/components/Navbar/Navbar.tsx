43cc3e28d27b99981938cb93dd9b009b8b278c07
Szymon <szymonngos@gmail.com>
2025-03-04 20:30:43 +0000

    init

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,24 @@
 import React from 'react';
+import { NavbarSignInLink } from './NavbarSignInLink';
+import { getClient } from '@/lib/apolloClient';
+import { getUserDataQuery } from '@/features/navbar/queries/getUserData';
+import { auth } from '@clerk/nextjs/server';
 
-export const Navbar = () => <div className="w-full bg-white py-6 border-b"> *** Test *** Navbar</div>;
+export const Navbar = async () => {
+  const { userId: clerkId } = await auth();
+
+  const client = getClient();
+  const { data } = await client.query({
+    query: getUserDataQuery,
+    variables: {
+      clerkId: clerkId,
+    },
+  });
+
+  return (
+    <div className="flex w-full bg-white py-6 border-b">
+      <div>Navbar Logo</div>
+      <div className="ml-auto">{data ? data.user.username : <NavbarSignInLink />}</div>
+    </div>
+  );
+};
