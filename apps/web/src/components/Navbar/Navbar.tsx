160287a72fe107e2306026ac4f493dbb72c27197
Szymon <szymonngos@gmail.com>
2025-03-14 21:51:05 +0000

    create page, add navbar links

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -3,6 +3,8 @@ import { NavbarSignInLink } from './NavbarSignInLink';
 import { auth, currentUser } from '@clerk/nextjs/server';
 import { SignedIn, SignedOut } from '@clerk/nextjs';
 import { UserMenu } from './UserMenu';
+import Link from 'next/link';
+import { navbarLinks } from '../config';
 
 export const Navbar = async () => {
   const user = await currentUser();
@@ -10,9 +12,18 @@ export const Navbar = async () => {
 
   return (
     <div className="flex w-full bg-white p-6 border-b">
-      <div>Navbar Logo</div>
+      <div className="flex justify-between">
+        <Link href="/">Navbar Logo</Link>
+      </div>
 
-      <div className="ml-auto">
+      <div className="flex gap-3 items-center ml-auto">
+        <ul className="flex gap-3">
+          {navbarLinks.map((link) => (
+            <li key={link.id}>
+              <Link href={link.url}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
         <SignedIn>
           <UserMenu userName={user?.username} clerkId={clerkId} />
         </SignedIn>
---
+++ apps/web/src/components/config.ts
@@ -0,0 +1,14 @@
+export const navbarLinks = [
+  {
+    id: 'plan_trip',
+    name: 'plan trip',
+    //todo: export url
+    url: '/trip/planner',
+  },
+  {
+    id: 'trips',
+    name: 'trips',
+    //todo: export url
+    url: '/trips',
+  },
+];
