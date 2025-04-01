66c28dd888d174767161b0ca9a160de79481504e
Szymon <szymonngos@gmail.com>
2025-04-01 16:23:49 +0100

    add query, display data

--- apps/web/src/app/user/[id]/page.tsx
+++ apps/web/src/app/user/[id]/page.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { getUserDataQuery } from '@/features/user/server/db/getUserDataQuery';
-import { getClient } from '@/lib/apolloClient';
+import { getUserTripsQuery } from '@/features/user/server/db/getUserTripsQuery';
+import { query } from '@/lib/apolloClient';
 
 const UserPage = async ({ params }: { params: { id: string } }) => {
-  const client = getClient();
-  const { data } = await client.query({
+  const { data: userData } = await query({
     query: getUserDataQuery,
+    variables: {
+      id: params.id.trim(),
+    },
+  });
+  const { data: tripsData } = await query({
+    query: getUserTripsQuery,
     variables: {
       id: params.id.trim(),
     },
   });
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
+    <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
       <div>
-        <p>{data?.user?.username}</p>
+        <p>{userData?.user?.username}</p>
+      </div>
+      <div className="w-full px-1 border-[0.5px] border-slate-200" />
+      <div className="w-full">
+        {tripsData?.trips?.map((trip) => (
+          <div key={trip.id} className="p-4 border rounded-lg mb-4">
+            <h3 className="font-semibold">{trip.title}</h3>
+            <p>From: {trip.origin}</p>
+            <p>To: {trip.destination}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
