27395e9ad9bd9b09798db7ee547b0d1f479221fe
Szymon <szymonngos@gmail.com>
2025-08-11 22:25:18 +0100

    add component

--- apps/web/src/features/googleMap/components/GoogleMapsContainer.tsx
+++ apps/web/src/features/googleMap/components/GoogleMapsContainer.tsx
@@ -3,14 +3,26 @@
 import React from 'react';
 import { GoogleMaps } from './GoogleMaps';
 import { useParams, usePathname } from 'next/navigation';
+import { TripDistanceInfo } from '@/features/trip/components/TripDistanceInfo/TripDistanceInfo';
+import { useGoogleMapsDirections } from '@/lib/contexts/DirectionsContext';
 
 export const GoogleMapsContainer = () => {
   const params = useParams();
   const pathname = usePathname();
   const tripId = params?.id as string;
+  const { distanceInfo } = useGoogleMapsDirections();
 
   const isTripViewPage = pathname.startsWith(`/trip/${tripId}`);
+  const isEditTripPlanner = pathname.startsWith('/trip/planner');
+  const isEditTripPlannerEdit = pathname.startsWith('/trip/planner/edit/');
   const canEdit = !isTripViewPage;
 
-  return <GoogleMaps canEdit={canEdit} />;
+  return (
+    <>
+      {distanceInfo && (isEditTripPlanner || isEditTripPlannerEdit) && (
+        <TripDistanceInfo distance={distanceInfo.distance} duration={distanceInfo.duration} />
+      )}
+      <GoogleMaps canEdit={canEdit} />
+    </>
+  );
 };
