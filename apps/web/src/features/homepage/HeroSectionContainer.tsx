a251b51c2db132ab981cc91740a7b225836143e5
Szymon <szymonngos@gmail.com>
2025-08-28 12:36:38 +0100

    cleanup

--- apps/web/src/features/homepage/HeroSectionContainer.tsx
+++ apps/web/src/features/homepage/HeroSectionContainer.tsx
@@ -1,15 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useScroll, useTransform } from 'framer-motion';
+
 import { HeroSection } from './HeroSection';
 
-export const HeroSectionContainer = () => {
-  const { scrollY } = useScroll();
-
-  const backgroundY = useTransform(scrollY, [0, 500], [0, -150]);
-  const overlayY = useTransform(scrollY, [0, 500], [0, -100]);
-  const textY = useTransform(scrollY, [0, 500], [0, -50]);
-
-  return <HeroSection backgroundY={backgroundY} overlayY={overlayY} textY={textY} />;
-};
+export const HeroSectionContainer = () => <HeroSection />;
