df8d47c6a4d1027843161c7a34eb90148b1a99cd
Szymon <szymonngos@gmail.com>
2025-08-29 13:19:06 +0100

    cleanup

--- apps/web/src/features/homepage/ProductBenefits.tsx
+++ apps/web/src/features/homepage/ProductBenefits.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Container } from '@/components/Container/Container';
 import { BenefitCard } from './BenefitCard';
 import { PRODUCT_BENEFITS_DATA } from './utils/homepageData';
-import Link from 'next/link';
-import { getTripPlannerUrl } from '../trip/helpers/getTripPlannerUrl';
 
 export const ProductBenefits = () => (
   <Container className="py-20">
@@ -24,15 +22,6 @@ export const ProductBenefits = () => (
           isNew={benefit.isNew}
         />
       ))}
-    </div>
-
-    <div className="text-center">
-      <Link
-        href={getTripPlannerUrl()}
-        className="w-fit px-8 py-4 bg-black text-white font-semibold rounded-lg hover:bg-gray-800 transition-colors flex items-center gap-2 mx-auto"
-      >
-        Get Started Now →
-      </Link>
     </div>
   </Container>
 );
