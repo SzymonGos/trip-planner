ce00644c010a1d7d6c405290def4ff8899295ed2
Szymon <szymonngos@gmail.com>
2025-09-02 11:13:47 +0100

    minor, pusle for aichat button

--- apps/web/src/features/trip/components/ViewTrip/TripTimeline.tsx
+++ apps/web/src/features/trip/components/ViewTrip/TripTimeline.tsx
@@ -7,21 +7,17 @@ type TTripTimelineProps = {
 
 export const TripTimeline: FC<TTripTimelineProps> = ({ origin, destination }) => (
   <div className="my-8 relative">
-    <div className="absolute left-4 top-0 bottom-0 w-0.5 bg-gray-300" />
-    <div className="space-y-6">
-      <div className="flex items-center gap-4">
-        <div className="w-8 h-8 bg-orange-500 rounded-full flex items-center justify-center flex-shrink-0 relative z-10">
-          <span className="text-white text-xs font-bold">A</span>
-        </div>
+    <div className="absolute left-[8px] top-4 bottom-4 w-0.5 border-l-4 border-dashed border-gray-500" />
+    <div className="space-y-12">
+      <div className="flex items-start gap-4">
+        <div className="w-5 h-5 border-[6px] border-gray-800 rounded-full flex items-center justify-center flex-shrink-0 relative z-10" />
         <div>
           <p className="text-sm text-gray-600 font-medium">Origin</p>
           <div className="text-gray-900">{origin}</div>
         </div>
       </div>
-      <div className="flex items-center gap-4">
-        <div className="w-8 h-8 bg-tp-red-100 rounded-full flex items-center justify-center flex-shrink-0 relative z-10">
-          <p className="text-white text-xs font-bold">B</p>
-        </div>
+      <div className="flex items-end gap-4">
+        <div className="w-5 h-5 border-[6px] border-gray-800 rounded-full flex items-center justify-center flex-shrink-0 relative z-10" />
         <div>
           <div className="text-sm text-gray-600 font-medium">Destination</div>
           <div className="text-gray-900">{destination}</div>
