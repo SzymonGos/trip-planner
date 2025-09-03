657d5794b6be496af16536b3146ca32dcc9b0fff
Szymon <szymonngos@gmail.com>
2025-09-03 19:59:32 +0100

    update pages

--- apps/web/src/app/(auth)/layout.tsx
+++ apps/web/src/app/(auth)/layout.tsx
@@ -1,9 +1,10 @@
+import { Footer } from '@/components/Footer/Footer';
 import React, { ReactNode } from 'react';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => (
-  <div className="fixed inset-0 flex justify-center items-center bg-gray-200 bg-opacity-50">
-    <div className="absolute inset-0 backdrop-blur-[2px] h-full w-full z-0" />
-    {children}
+  <div className="h-svh bg-tp-white-100 flex flex-col">
+    <div className="flex items-center justify-center px-4">{children}</div>
+    <Footer />
   </div>
 );
 
