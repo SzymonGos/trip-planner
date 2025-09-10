b66fd76eb3ff6270ccd1213c02dc09bc0fd8dcc0
Szymon <szymonngos@gmail.com>
2025-09-10 09:33:42 +0100

    add force-dynamic

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,8 @@ import { HeroSectionContainer } from '@/features/homepage/HeroSectionContainer';
 import { HowItWorks } from '@/features/homepage/HowItWorks';
 import { ProductBenefits } from '@/features/homepage/ProductBenefits';
 
+export const dynamic = 'force-dynamic';
+
 const Home = () => (
   <main>
     <div className="pt-10 bg-gradient-to-b from-zinc-400 to-gray-50 to-80%">
--- apps/web/src/app/trips/page.tsx
+++ apps/web/src/app/trips/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { TripsLlistContainer } from '@/features/trip/components/TripsLlistContainer/TripsLlistContainer';
 import { Footer } from '@/components/Footer/Footer';
 
+export const dynamic = 'force-dynamic';
+
 const TripsPage = () => (
   <div>
     <TripsLlistContainer />
